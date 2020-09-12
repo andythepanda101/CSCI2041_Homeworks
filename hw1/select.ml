33dcc62a2ea205a9d8fe3bdafb2781579b059b38
Nick Hopper <hopper@cs.umn.edu>
2020-09-11 20:25:09 -0500

    added usage message

--- hw1/select.ml
+++ hw1/select.ml
@@ -10,7 +10,16 @@ let rec select arglist id od sortcol srev hr outcols outrows = match arglist wit
 | "-rs"::c::t -> select t id od c       true hr outcols outrows
 | "-hr"::t -> select t id od sortcol srev true outcols outrows
 | "-c"::c::t -> select t id od sortcol srev hr c outrows
-| _ -> failwith "select: invalid argument format"
+| _ -> print_string {usg|
+select: sort, project, and cut csv-style tables. available options:
+        -id c        use c as the input column delimiter
+        -od c        use c as the output column delimiter
+        -k  n        keep only the first n rows of output
+        -s  c        sort using column c as the key
+        -rs c        reverse sort (descending order) using column c as the key
+        -hr          table has a header row
+        -c  c1,..,cn select columns c1,..,cn from the table for output.  Use "*" for all
+|usg}
 
 
 (* make the call! *)
